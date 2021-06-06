76de0e768d415d50cd1ab6c39d28fb007d62c38f
nicorenaldo <nicorenaldo14@gmail.com>
2021-06-06 23:41:36 +0700

    added download limiter

--- core/utils.py
+++ core/utils.py
@@ -36,7 +36,7 @@ def download(class_name, args):
     # construct path to destination dir
     dst_dir_root = args.destination_dir if args.destination_dir is not None else DEFAULT_DEST_DIR
     dst_dir = os.path.join(dst_dir_root, class_name)  # Create directory to store found files
-
+    count = 0
     print("dst_dir: " + dst_dir)
 
     if not os.path.isdir(dst_dir):
@@ -50,9 +50,12 @@ def download(class_name, args):
             # print command for debugging
             print("ffmpeg -ss " + str(row[1]) + " -t 10 -i $(youtube-dl -f 'bestaudio' -g https://www.youtube.com/watch?v=" +
                        str(row[0]) + ") -ar " + str(DEFAULT_FS) + " -- \"" + dst_dir + "/" + str(row[0]) + "_" + row[1] + ".wav\"")
-            os.system(("ffmpeg -ss " + str(row[1]) + " -t 10 -i $(youtube-dl -f 'bestaudio' -g https://www.youtube.com/watch?v=" +
+            exitcode = os.system(("ffmpeg -ss " + str(row[1]) + " -t 10 -i $(youtube-dl -f 'bestaudio' -g https://www.youtube.com/watch?v=" +
                        str(row[0]) + ") -ar " + str(DEFAULT_FS) + " -- \"" + dst_dir + "/" + str(row[0]) + "_" + row[1] + ".wav\""))
-
+            if(exitcode == 0):
+                count += 1
+            if(count >= args.limit):
+                break
 
 def create_csv(class_name, args):
     """
